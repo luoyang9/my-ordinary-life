d50975ec8687a31b1b1609d394b77a942d4ebf24
Charlie Zhang <peidong97@yahoo.ca>
2017-06-07 02:08:07 -0400

    change root

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use("/api/events", checkUser, eventApi);
 app.use("/api/users", checkUser, userApi);
 
 app.get('/', function(req, res) {
-    res.sendFile('index.html',  { root: "dist/"});
+    res.sendFile('index.html',  { root: path.resolve(__dirname, "../dist/")});
 });
 
 // start app
