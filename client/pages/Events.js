ff776761c467a79843480b0ae4d6f04db2865fcb
Charlie Zhang <peidong97@yahoo.ca>
2017-06-07 00:57:41 -0400

    polish up the experience

--- client/pages/Events.js
+++ client/pages/Events.js
@@ -12,24 +12,37 @@ export default class Events extends React.Component {
 		super(props);
 
 		this.state = {
+			initialTitle: "",
+			initialContent: "",
 			title: "",
 			content: "",
-			message: ""
+			message: "",
+			saveTimer: null
 		}
 
 		this.onTitleChange = (evt) => this.setState({message: "", title: evt.target.value})
-		this.onContentChange = (evt) => this.setState({message: "", content: evt.target.value})
+		this.onContentChange = (evt) => {
+			clearTimeout(this.state.saveTimer)
+			this.setState({message: "Writing...", content: evt.target.value, saveTimer: setTimeout(this.submitEvent, 1000)})
+		};
 		this.onContentInput = (evt) => {
-	  	evt.target.style.height = "";
-		  evt.target.style.height = Math.min(evt.target.scrollHeight + 10, 300) + "px";
+	  	// evt.target.style.height = "";
+		  // evt.target.style.height = evt.target.scrollHeight + "px";
 		};
 
 		this.submitEvent = (evt) => {
-			evt.preventDefault();
-			evt.stopPropagation();
+			if(evt) {
+				evt.preventDefault();
+				evt.stopPropagation();
+			}
 
 			if(!this.state.title || !this.state.content) {
 				this.setState({message: "Please fill out all fields. I wanna hear about your day! :)"});
+				return;
+			}
+
+			if(this.state.title === this.state.initialTitle && this.state.content === this.state.initialContent) {
+				this.setState({message: ""});
 				return;
 			}
 
@@ -45,7 +58,7 @@ export default class Events extends React.Component {
 					console.error(event.err);
 					this.setState({message: "Failed to save. Try again?"});
 				} else {
-					this.setState({message: "Saved!"});
+					this.setState({message: "Saved!", initialTitle: this.state.title, initialContent: this.state.content});
 				}
 			});	
 
@@ -66,7 +79,9 @@ export default class Events extends React.Component {
 			if(event && event.err) {
 				console.error(event.err);
 			} else {
-				this.setState({title: event ? event.title : "", content: event ? event.content : ""});
+				const title = event ? event.title : "";
+				const content = event ? event.content : "";
+				this.setState({title: title, initialTitle: title, content: content, initialContent: content});
 			}
 		});
 	}
@@ -80,10 +95,10 @@ export default class Events extends React.Component {
 						onChange={this.onTitleChange} value={this.state.title} />}
 					>
 						<Input type="textarea" className="new_event_content" placeholder="What happened today?" 
-							rows={4} onInput={this.onContentInput} onChange={this.onContentChange} value={this.state.content} />
+							rows={4} onInput={this.onContentInput} onChange={this.onContentChange} 
+							value={this.state.content} autosize={true}/>
+						<p style={{float: "right", margin: "4px 0"}}>{this.state.message}</p>
 					</Card>
-					<p>{this.state.message}</p>
-					<Button className="new_event_save" type="submit" onClick={this.submitEvent}>Save</Button>
 				</form>
 			</div>
 		);
@@ -94,7 +109,7 @@ export default class Events extends React.Component {
 			return <div className="event_container" key={event.id}>
 				<Card className="event_card" title={event.title}>
 					<p>{event.content}</p>
-					<p>{moment(event.publishDate).format("YYYY/MM/DD")}</p>
+					<p style={{float: "right", margin: "10px 0"}}>{moment(event.publishDate).format("MMM D")}</p>
 				</Card>
 			</div>
 		});
